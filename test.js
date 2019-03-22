1adafc4f97ad4b9b48f17a6a7fe465e6d5378091
khafradev <me@khafra.club>
2019-03-22 19:01:45 -0400

    Fixes and better practices

--- test.js
+++ test.js
@@ -1,5 +1,21 @@
 const lyrics = require('./index');
-lyrics.azlyrics.getURL('good morning good morning')
+lyrics.musixmatch.getURL('more than a feeling')
+    .then(r => lyrics.musixmatch.getLyrics(r))
+    .then(() => console.log('musixmatch lyrics obtained'))
+    .catch(console.error);
+
+lyrics.azlyrics.getURL('queen you\'re my best friend')
     .then(r => lyrics.azlyrics.getLyrics(r))
-    .then(console.log)
+    .then(() => console.log('azlyrics lyrics obtained'))
+    .catch(console.error);
+
+lyrics.lyricscom.getURL('limelight')
+    .then(r => lyrics.lyricscom.getLyrics(r))
+    .then(() => console.log('lyrics.com lyrics obtained'))
+    .catch(console.error)
+
+lyrics.lyricslive.getURL('don\'t stop')
+    .then(r => lyrics.lyricslive.getLyrics(r))
+    .then(() => console.log('lyricslive lyrics obtained'))
+    .catch(console.error)
 
