c9e039f0e3dea94844a68e9620e159f890f5971b
KhafraDev <42794878+KhafraDev@users.noreply.github.com>
2019-12-09 13:45:49 -0500

    2.0.0-rewrite
    not suitable for use yet

--- test.js
+++ test.js
@@ -1,20 +1,8 @@
-const lyrics = require('./index');
-lyrics.musixmatch.getURL('more than a feeling')
-    .then(r => lyrics.musixmatch.getLyrics(r))
-    .then(() => console.log('musixmatch lyrics obtained'))
-    .catch(console.error);
+const { MusixMatch } = require('./lib/musixmatch');
 
-lyrics.azlyrics.getURL('queen you\'re my best friend')
-    .then(r => lyrics.azlyrics.getLyrics(r))
-    .then(() => console.log('azlyrics lyrics obtained'))
-    .catch(console.error);
+const m = new MusixMatch();
 
-lyrics.lyricscom.getURL('limelight')
-    .then(r => lyrics.lyricscom.getLyrics(r))
-    .then(() => console.log('lyrics.com lyrics obtained'))
-    .catch(console.error);
-
-lyrics.lyricslive.getURL('don\'t stop')
-    .then(r => lyrics.lyricslive.getLyrics(r))
-    .then(() => console.log('lyricslive lyrics obtained'))
-    .catch(console.error);
+(async () => {
+    const r = await m.fetchURL('Boston more than a feeling');
+    console.log(r);
+})();
